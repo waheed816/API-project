dc1bd1e449ed2ef173e5e0eb2eb4baba8c0ad62e
Mehedi Meem <waheed816@gmail.com>
2023-01-22 17:42:31 -0500

    Added firstName and lastName column to Users table

---
+++ backend/db/migrations/20230122211233-add-firstName-and-lastName-to-user.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return [
+      await queryInterface.addColumn('Users', 'firstName', {
+        type: Sequelize.STRING,
+        //allowNull: false
+      }),
+      await queryInterface.addColumn('Users', 'lastName', {
+        type: Sequelize.STRING,
+        //allowNull: false
+      }),
+    ];
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return [
+      await queryInterface.removeColumn('Users', 'firstName'),
+      await queryInterface.removeColumn('Users', 'lastName'),
+    ];
+  }
+};
