2c845c8b85a98e6e492b70a1e595d05116acde26
Mehedi Meem <waheed816@gmail.com>
2023-01-25 02:28:25 -0500

    Created SpotImage model and demo-spotImage seed

---
+++ backend/db/seeders/20230125071716-demo-spotImage.js
@@ -0,0 +1,63 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    options.tableName = 'SpotImages';
+    return queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        url: "url image 1",
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: "url image 2",
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: "url image 3",
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: "url image 4",
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: "url image 5",
+        preview: true
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'SpotImages'
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3, 4, 5] }
+    }, {});
+  }
+};
