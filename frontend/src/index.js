caf95a4d7eff432c518dfe66d7d27deadb1626af
Mehedi Meem <waheed816@gmail.com>
2023-03-10 20:16:19 -0500

    completed phase 1 session action and reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,22 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import configureStore from './store';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
+// if (process.env.NODE_ENV !== 'production') {
+//   window.store = store;
+// }
+
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
